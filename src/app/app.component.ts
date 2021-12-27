83076084c6a5cd193f220a864fe3ba88e038c80c
Deividwb <bernardinodeivid@gmail.com>
2021-12-27 19:28:23 -0300

    creating class courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//referenciando o index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
